105f47eee5f79c02e99f14305d2b9c9ab90205cc
Vinícius Padilha <viniciuspadilha@MacAir-de-Vinicius.local>
2019-06-24 13:47:42 -0300

    Ajuste

--- src/index.js
+++ src/index.js
@@ -18,10 +18,12 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
-server.listen(process.env.PORT || 3000, () => console.log('API on!'));
+app.listen(process.env.PORT || 3000);

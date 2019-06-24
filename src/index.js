592b02f5e9c2e5d21e2af051869123e01f7e8304
Vinícius Padilha <viniciuspadilha@MacAir-de-Vinicius.local>
2019-06-24 14:13:47 -0300

    Ajuste api

--- src/index.js
+++ src/index.js
@@ -12,17 +12,17 @@ mongoose.connect('mongodb+srv://admin:admin@cluster0-ms0ca.mongodb.net/test?retr
     useNewUrlParser: true,
 });
 
-app.use((req, res, next) => {
-    req.io = io;
+// app.use((req, res, next) => {
+//     req.io = io;
 
-    next();
-});
+//     next();
+// });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+// app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
 
 app.use(require('./routes'));
 
